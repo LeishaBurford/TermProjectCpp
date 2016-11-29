baef32bc3ee1129ae3c1e422db28d353a9ba7846
Leisha Burford <leishabobisha@gmail.com>
2016-11-28 23:13:23 -0500

    changed sell() in chain class, not tested

--- Bohnanza/Bohnanza/chain.h
+++ Bohnanza/Bohnanza/chain.h
@@ -11,21 +11,37 @@ class Chain_Base {};
 template <class T>
 class Chain : public Chain_Base {
     friend std::ostream &operator<<(std::ostream& out, const Chain<T>& chain);
-    std::vector<T> chain;
-    typename std::vector<T>::value_type type;
+    std::vector<T*> chain;
 public:
-    Chain<T>() = default;    //default constructor
-    Chain( const std::istream&, CardFactory* ); // TODO
-    Chain<T>& operator+=(T*);
-    int sell();
+    Chain<T>() = default;
+    Chain( const std::istream&, CardFactory* ){}; // TODO
+    
+    // adds a card to the Chain. If the run-time type does not match the template type of the chain and exception of type IllegalType needs to be raised.
+    Chain& operator+=(T* card) {
+        chain.push_back(card);
+        return *this;
+    }
+    
+    //counts the number cards in the current chain and returns the number coins according to the function Card::getCardsPerCoin. TODO check if working
+    //when called, update player num active coins
+    int sell() {
+        if(!chain.empty()){
+            T* card = dynamic_cast<T*>(chain.back());
+            for(int coins = 4; ;coins--) {
+                int neededCards = card->getCardsPerCoin(coins);
+                if(chain.size() >= neededCards) return coins;
+            }
+        }
+        return 0;
+    }
 };
 
-inline std::ostream& operator<< (std::ostream& o, const Chain<Card*>& _chain)
+template <typename T>
+inline std::ostream& operator<< (std::ostream& o, const Chain<T>& _chain)
 {
-    
     if(!_chain.chain.empty()){
         auto card = _chain.chain.back();
-        o << card->getName() << " ";
+        o <<  card->getName() << " ";
         for(int i = 0; i < _chain.chain.size(); ++i) {
             o << *(_chain.chain.at(i)) << " ";
         }
@@ -34,41 +50,6 @@ inline std::ostream& operator<< (std::ostream& o, const Chain<Card*>& _chain)
     return o;
 }
 
-//Chain methods
-
-
-//TODO
-// The Chain needs a constructor which accepts an istream and reconstructs the chain from file when a game is resumed.
-template <class T>
-Chain<T>::Chain( const std::istream&, CardFactory* ){}
-
-// adds a card to the Chain. If the run-time type does not match the template type of the chain and exception of type IllegalType needs to be raised.
-template<class T>//method calling this must handle exception
-Chain<T>& Chain<T>::operator+=( T* card){
-    try {
-        //auto _card = dynamic_cast<Card*>(card);
-        //std::cout << typeid(type).name() << " " << typeid(card).name() << std::endl;
-        if(typeid(type) != typeid(card)) {
-            throw std::invalid_argument("Cannot add card to this chain");
-        }
-        chain.push_back(*card);
-    } catch(const std::invalid_argument& e) {
-        std::cout << e.what() << std::endl;
-    }
-    return *this;
-}
-//counts the number cards in the current chain and returns the number coins according to the function Card::getCardsPerCoin. TODO check if working
-//when called, update player num active coins
-template<class T>
-int sell(){
-    int total = 0;
-    while(Chain<T>::chain.back()) {
-        total += Chain<T>::chain.back()->getCardsPerCoin(Chain<T>::chain.size());
-        Chain<T>::chain.pop_back();
-    }
-    return total;
-}
-
-template class Chain<Card*>;
+template class Chain<Card>;
 
 #endif /* chain_h */
--- Bohnanza/Bohnanza/player.h
+++ Bohnanza/Bohnanza/player.h
@@ -4,43 +4,43 @@
 #include <string>
 #include <iostream>
 #include <vector>
-//#include "chain.h"
+#include "chain.h"
 
-//class Player {
-//    friend ostream &operator<<(ostream& out, const Player& player);
-//    void print(const ostream& out) const;
-//    Chain<Card*>& getChain(int i){return chains.at(i);}
-//    std::string name;
-//    int numCoins;
-//    Hand hand;
-//    int maxChains;
-//    int activeChains;
-//    //std::vector<Chain<T*>> chains;
-//public:
-//    
-//    std::string getName() const;
-//    Player( std::string& _name) : name(_name), numCoins(0), maxChains(2), activeChains(0){
-//        chains.resize(2);
-//    }
-//    Player( const std::istream&, CardFactory* );
-//    int getMaxNumChains();
-//    int getNumChains();
-//    int getNumCoins();
-//    Player& operator+=( int );
-//    template<class T>
-//    Chain<T>& operator[](int i) {
-//        return getChain(i);
-//    }
-//    void buyThirdChain();
-//    void printHand(std::ostream&, bool);
-//   
-//};
-//
-//inline std::ostream& operator<< (std::ostream& o, const Player& player)
-//{
-//    player.print(o);
-//    return o;
-//}
-//
-//
+class Player {
+    friend ostream &operator<<(ostream& out, const Player& player);
+    void print(const ostream& out) const;
+    Chain_Base& getChain(int i){return chains.at(i);}
+    std::string name;
+    int numCoins;
+    Hand hand;
+    int maxChains;
+    int activeChains;
+    std::vector<Chain_Base> chains;
+public:
+    
+    std::string getName() const;
+    Player( std::string& _name) : name(_name), numCoins(0), maxChains(2), activeChains(0){
+        chains.resize(2);
+    }
+    Player( const std::istream&, CardFactory* );
+    int getMaxNumChains();
+    int getNumChains();
+    int getNumCoins();
+    Player& operator+=( int );
+    template<class T>
+    Chain<T>& operator[](int i) {
+        return getChain(i);
+    }
+    void buyThirdChain();
+    void printHand(std::ostream&, bool);
+   
+};
+
+inline std::ostream& operator<< (std::ostream& o, const Player& player)
+{
+    player.print(o);
+    return o;
+}
+
+
 #endif /* player_h */
